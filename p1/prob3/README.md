440312233c15cef6fa8de60d72b12da84f9a34ee
정 세미 <smjeong53@c4r2s7.codyssey.kr>
2025-07-12 00:22:57 +0900

    add: prob3

---
+++ p1/prob3/README.md
@@ -0,0 +1,7 @@
+1. 음성출력 웹 애플리케이션
+    - gtts란?
+    - requeset.args.get()
+    - BytesIO()
+    - gTTS().write_to_fp
+    - Response()
+        - mimetype
---
+++ p1/prob3/david/calculator.py
@@ -0,0 +1,59 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise Exception("Error: Division by zero.")
+    return a / b
+
+ops = {"+": add, "-": subtract, "*": multiply, "/": divide}
+
+def validateInput(a, b, op):
+    try:
+        numA = float(a)
+        numB = float(b)
+    except:
+        raise Exception("Invalid Input: Not a float")
+    if op not in ops.keys():
+        raise Exception("Invalid operator.")
+    return (numA, numB, op)
+
+def getInput():
+    arr = input("input a b: ").split()
+    if len(arr) != 2:
+        raise Exception("Invalid Input: format should be 'a b'")
+    op = input("input operator: ")
+    return validateInput(arr[0], arr[1], op)
+
+def getBonusInput():
+    arr = input("input a <operator> b: ").split()
+    if len(arr) != 3:
+        raise Exception("Invalid Input: format should be 'a operator b'")
+    return validateInput(arr[0], arr[2], arr[1])
+    
+def selectType(ans):
+    if ans == "1":
+            a, b, op = getInput()
+    elif ans == "2":
+        a, b, op = getBonusInput()
+    elif ans == "3":
+        exit(0)
+    else:
+        print("Wrong input")
+        return
+    print(f"Result: <{ops[op](a, b)}>")
+
+if __name__ == "__main__":
+    while True:
+        ans = input("select type of input (1 - default, 2 - bonus, 3 - exit): ")
+        try:
+            selectType(ans)
+        except Exception as e:
+            print(e)
+        
